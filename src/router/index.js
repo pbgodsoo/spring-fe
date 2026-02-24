50b99e22f3748f4962589ef718aac7ebae32097b
Beomsoo <linoleca@naver.com>
2026-02-24 16:15:49 +0900

    [feat] 회원가입 기능 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Board from '@/views/Board.vue'
 import BoardDetail from '@/views/BoardDetail.vue'
+import Signup from '@/views/Signup.vue'
 
 const routes = [
   {
@@ -9,6 +10,7 @@ const routes = [
     component: Board,
   },
   { path: '/board/:idx', name: 'BoardDetail', component: BoardDetail },
+  { path: '/signup', name: 'Signup', component: Signup },
 ]
 
 const router = createRouter({
